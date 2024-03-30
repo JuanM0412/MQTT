a6d65d61a2ca75e4807cd49de01b74e65d436125
JuanM0412 <juangomezp1204@gmail.com>
2024-03-30 16:41:51 -0500

    SUBSCRIBE packet

--- src/client.c
+++ src/client.c
@@ -6,25 +6,58 @@
 #include <strings.h>
 #include <sys/socket.h>
 #include <unistd.h>
-<<<<<<< HEAD:Client.c
-#include "Packets/packet.c"
-=======
 #include "../include/encode.h"
 #include "../include/decode.h"
 #include "../include/packet.h"
->>>>>>> 9ae1787 (Project structure):src/client.c
 
 #define MAX 80
-#define CONFIG_FILE "config.txt" // Configuration file name
+#define CONFIG_FILE "../config.txt"
 #define SA struct sockaddr
 
-// Function to send a CONNECT packet to the server
-void send_connect_packet(int sockfd) {
-    MQTT_Packet connect_packet = create_connect_packet(0, "client");
+void send_packet_connect(int sockfd, MQTT_Packet packet) {
+    printf("Fixed Header: %u\n", packet.fixed_header);
+    printf("Remaining Length: %u\n", packet.remaining_length);
     
-    write(sockfd, &connect_packet, sizeof(connect_packet));
+    // Calcular el tamaño total del paquete
+    size_t total_size = 1 + sizeof(packet.remaining_length) +
+                        packet.remaining_length;
 
-    free_packet(&connect_packet);
+    // Crear un búfer para almacenar el paquete MQTT
+    unsigned char buffer[total_size];
+    size_t offset = 0;
+
+    // Copiar el encabezado fijo en el búfer
+    buffer[offset++] = packet.fixed_header;
+
+    // Copiar la longitud restante en el búfer
+    buffer[offset++] = packet.remaining_length;
+
+    // Copiar el encabezado variable en el búfer
+    memcpy(buffer + offset, packet.variable_header, packet.remaining_length);
+    offset += packet.remaining_length;
+
+    // Enviar el paquete a través del socket
+    ssize_t bytes_sent = send(sockfd, buffer, total_size, 0);
+    if (bytes_sent < 0) {
+        perror("Error sending packet to server");
+        exit(EXIT_FAILURE);
+    } else if (bytes_sent != total_size) {
+        fprintf(stderr, "Incomplete packet sent to server\n");
+        exit(EXIT_FAILURE);
+    } else {
+        printf("Packet sent successfully\n");
+    }
+}
+
+void send_packet_subscribe(int sockfd, MQTT_Packet packet) {
+    printf("Fixed Header: %u\n", packet.fixed_header);
+    printf("Remaining Length: %u\n", packet.remaining_length);
+
+    // Calculate total size of the packet
+    size_t total_size = sizeof(packet.fixed_header) + sizeof(packet.remaining_length) +
+                        packet.remaining_length + sizeof(packet.payload);
+
+    free_packet(&packet);
 }
 
 void send_publish_packet(int sockfd) {
@@ -38,6 +71,37 @@ void send_publish_packet(int sockfd) {
     write(sockfd, buffer, sizeof(buffer));
 
     free_packet(&publish_packet);
+}
+
+void send_packet_to_server(int sockfd, MQTT_Packet packet) {
+    printf("Fixed Header: %u\n", packet.fixed_header);
+    printf("Remaining Length: %u\n", packet.remaining_length);
+    // Calculate total size of the packet
+    size_t total_size = sizeof(packet.fixed_header) + sizeof(packet.remaining_length) +
+                        packet.remaining_length + sizeof(packet.payload);
+
+    // Serialize structure data into a byte buffer
+    unsigned char buffer[total_size];
+    size_t offset = 0;
+
+    // Copy structure fields into buffer
+    memcpy(buffer + offset, &packet.fixed_header, sizeof(packet.fixed_header));
+    offset += sizeof(packet.fixed_header);
+
+    memcpy(buffer + offset, &packet.remaining_length, sizeof(packet.remaining_length));
+    offset += sizeof(packet.remaining_length);
+
+    memcpy(buffer + offset, packet.variable_header, packet.remaining_length);
+    offset += packet.remaining_length;
+
+    memcpy(buffer + offset, packet.payload, sizeof(packet.payload));
+
+    // Send the buffer through the socket
+    write(sockfd, buffer, total_size);
+
+    while(1){
+        ;;
+    }
 }
 
 int main() {
@@ -86,9 +150,9 @@ int main() {
     } else
         printf("connected to the server..\n");
 
-    MQTT_Packet publish_packet = create_publish_packet(encodeMessageToUTF8("EAFIT/Sede/Poblado/Bloque/33/Salon/301/humedad"), encodeMessageToUTF8("33%"));
+    MQTT_Packet packet = create_subscribe_packet(encodeMessageToUTF8("EAFIT/Sede/Poblado/Bloque/33/Salon/301/humedad"));
     // Send the packet to the server
-    send_packet_to_server(sockfd, publish_packet);
+    send_packet_subscribe(sockfd, packet);
 
     // Close the socket
     close(sockfd);
--- src/server.c
+++ src/server.c
@@ -14,9 +14,8 @@
 #include "../include/packet.h"
 #include "../include/tree.h"
 
-#define MAX 80 
-#define CONFIG_FILE "config.txt" // Configuration file name
-#define SA struct sockaddr 
+#define MAX 180
+#define SA struct sockaddr
 
 // Función para recibir y procesar un paquete MQTT
 void handle_mqtt_packet(char *buffer) {
@@ -28,28 +27,58 @@ void handle_mqtt_packet(char *buffer) {
     printf("Message: %s\n", received_packet.payload);
 }
 
-// Función para el hilo de cliente
-void *func(void *arg) {
+// Function designed for chat between client and server
+void *func(void *arg) { 
     int connfd = *((int*)arg);
-    char buff[sizeof(MQTT_Packet)]; 
-    int n; 
+    
+    // Receive packet from client
+    MQTT_Packet received_packet = receive_packet_from_client(connfd);
 
-    for (;;) {
-        bzero(buff, sizeof(buff));
-        read(connfd, buff, sizeof(buff));
-
-        // Procesar el paquete MQTT recibido
-        handle_mqtt_packet(buff);
-
-        // Envía una respuesta al cliente si es necesario
-        // write(connfd, response, sizeof(response));
-
-        if (strncmp("exit", buff, 4) == 0) {
-            printf("Server Exit...\n");
-            break;
-        }
+    printf("Variable Header: ");
+    for (int i = 0; i < received_packet.remaining_length; i++) {
+        printf("%02X ", received_packet.variable_header[i]);
     }
+    printf("\n");
 
+    printf("Payload: ");
+    for (int i = 0; i < received_packet.remaining_length; i++) {
+        printf("%02X ", received_packet.payload[i]);
+    }
+    printf("\n");
+
+    // Concatenate variable header and payload into a single buffer
+    size_t total_length = received_packet.remaining_length + strlen(received_packet.payload);
+    unsigned char *serialized_packet = malloc(total_length);
+    memcpy(serialized_packet, received_packet.variable_header, received_packet.remaining_length);
+    memcpy(serialized_packet + received_packet.remaining_length, received_packet.payload, strlen(received_packet.payload));
+
+    // Print serialized packet
+    printf("Serialized packet: ");
+    for (int i = 0; i < total_length; ++i) {
+        printf("%02X ", serialized_packet[i]);
+    }
+    printf("\n");
+
+    // Decode topic encoded in UTF-8
+    unsigned int topic_length = (received_packet.variable_header[0] << 8) | received_packet.variable_header[1];
+    char *decoded_topic = decodeUTF8(serialized_packet + 2); // Ignore variable header
+    printf("Decoded topic: %s\n", decoded_topic);
+
+    // Decode message encoded in UTF-8 (ignoring variable header)
+    char *decoded_message = decodeUTF8(serialized_packet + received_packet.remaining_length); // Ignore variable header
+    printf("Decoded message: %s\n", decoded_message);
+
+    // Concatenate topic and message using strcat
+    char *topic_message = malloc(strlen(decoded_topic) + strlen(decoded_message) + 2); // +2 for space and null terminator
+    strcpy(topic_message, decoded_topic);
+    strcat(topic_message, " ");
+    strcat(topic_message, decoded_message);
+
+    // Free memory
+    free(received_packet.variable_header);
+    free(received_packet.payload);
+
+    // Close connection with this client
     close(connfd);
     return NULL;
 }
@@ -114,6 +143,7 @@ int main(int argc, char *argv[]) {
             printf("server accept the client...\n"); 
         
         pthread_t tid; // Thread ID
+        printf("Thread ID: %lu\n", (unsigned long)tid);
         pthread_create(&tid, NULL, func, &connfd); // Create a thread to handle this connection
     }
 
