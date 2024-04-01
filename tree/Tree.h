e158a89d83c2126f894960bd8d00176cbf37ca25
Miguel Hoyos <mahoyosv@eafit.edu.co>
2024-03-31 21:28:41 -0500

    PUB and SUB functions.

--- tree/Tree.h
+++ tree/Tree.h
@@ -7,11 +7,15 @@ typedef struct TreeNode {
     int num_children;
     char **messages;
     int num_messages;
+    int *users; 
+    int num_users; 
 } TreeNode;
 
 TreeNode* createTreeNode(const char *name);
 void freeTreeNode(TreeNode *node);
-void insertMessage(TreeNode *root, const char *input);
+void insertMessage(TreeNode *root, const char *topic, const char *message);
 void printTree(TreeNode *node, int depth);
+void PUB(TreeNode *root, const char *topic, const char *message); 
+void SUB(TreeNode *root, const char *topic, int user); 
 
 #endif /* TREE_H */
